5edc19ad5f9e0df2ad8c046577b769a659ea8152
Vikas Kumar <110839417+VikasKumarGit@users.noreply.github.com>
2022-12-13 13:30:42 +0530

    Add files via upload

---
+++ assets/globel.js
@@ -0,0 +1,9 @@
+
+
+function writeName(){
+    var pName=document.querySelector('p');
+    //document.write(inputName);
+    console.log(pName);
+    // var p=document.createElement('p');
+    // pName=document.createElement('p');
+}
